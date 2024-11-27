bec0161e69aecc19c4f2086aff7720207e8c07bb
Maxime Baude <contact.mbaude@gmail.com>
2024-11-27 16:51:17 +0100

    jeu terminé 👍

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
-import Square from "./square"
+import Board from "./board"
 
 export default function Page() {
   return (
-    <div>
-      <h1>Hello, Next.js!</h1>
-      <Square />
+    <div className="container w-1/2 mx-auto border border-blue-700 p-3">
+      <h1 className="italic">Hello, Next.js!</h1>
+      <Board />
     </div>
   );
 }
